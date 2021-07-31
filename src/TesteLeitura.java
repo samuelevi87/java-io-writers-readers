41cdb0d6330d6736e24e6e221e0742ab7a5cb076
Samuel Levi <samuel.levi.alves@outlook.com>
2021-07-31 16:21:15 -0300

    Iterando linhas

--- src/TesteLeitura.java
+++ src/TesteLeitura.java
@@ -2,19 +2,22 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Objects;
 
 public class TesteLeitura {
 
 	public static void main(String[] args) throws IOException {
-		
-		//Fluxo de entrada com arquivo
-		
+
+		// Fluxo de entrada com arquivo
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("texto-ti.txt")));
-		
+
 		String line = br.readLine();
-		
-		System.out.println(line);
-		
+
+		while (Objects.nonNull(line)) {
+			System.out.println(line);
+			line = br.readLine();
+		}
 		br.close();
 	}
 
