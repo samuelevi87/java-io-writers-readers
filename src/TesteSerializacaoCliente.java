6f9677c8d0e579f19c7f407c55bb987c81522826
Samuel Levi Araújo Alves <samuel.levi@senior.com.br>
2021-08-04 08:44:50 -0300

    Serialização

---
+++ src/TesteSerializacaoCliente.java
@@ -0,0 +1,25 @@
+
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class TesteSerializacaoCliente {
+
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+		
+		Cliente cliente = new Cliente();
+		cliente.setNome("Samuel");
+		cliente.setProfissao("Dev");
+		cliente.setCpf("11122233344");
+		
+		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("cliente.bin"));
+		oos.writeObject(cliente);
+		oos.close();
+	
+//		ObjectInputStream ois = new ObjectInputStream(new FileInputStream("cliente.bin"));
+//		Cliente cliente = (Cliente) ois.readObject();
+//		ois.close();
+//		System.out.println(cliente.getCpf());
+	}
+}
